eb3c979a3702b804c83e47e93950ee52d0faf4fa
rkdev19 <109154244+rkdev19@users.noreply.github.com>
2025-03-22 01:46:49 +0530

    Delete tradnigbot directory

--- tradnigbot/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Currency(models.Model):
-    symbol = models.CharField(max_length=10, primary_key=True)
-    usd_value = models.DecimalField(max_digits=10, decimal_places=2, default=1.0)
